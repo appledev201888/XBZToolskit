a8697324304f0b173895bb4a418030b78c15bb79
chenweibin <weibinchen@uutaka.com>
2019-01-01 16:26:41 +0800

    123

---
+++ Classes/XBZToolKits.framework/Headers/XBZToolKits.h
@@ -0,0 +1,14 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XBZToolKits.
+FOUNDATION_EXPORT double XBZToolKitsVersionNumber;
+
+//! Project version string for XBZToolKits.
+FOUNDATION_EXPORT const unsigned char XBZToolKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XBZToolKits/PublicHeader.h>
+
+#import <XBZToolKits/XBZToolKitsManage.h>
+
+
