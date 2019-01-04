19676645ed5032e80a6b37b5c2ff9ac5d844d63a
chenweibin <weibinchen@uutaka.com>
2019-01-04 16:00:35 +0800

    提交的描述说明

--- Classes/XBZToolKits.framework/Headers/XBZToolKitsManage.h
+++ Classes/XBZToolKits.framework/Headers/XBZToolKitsManage.h
@@ -1,12 +1,17 @@
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface XBZToolKitsManage : NSObject
 
-+ (void)XBZToolKitsManageWithAid:(NSString *)aid
-                               k:(NSString *)k;
++ (XBZToolKitsManage *)sharedManager;
+
+- (void)XBZToolKitsbegin:(UIView *)view;
+
+- (void)XBZToolKitsend:(UIView *)view;
+
 @end
 
 NS_ASSUME_NONNULL_END
